c6edebe1d64cdf44066259d368b8bfe58b9fb8b6
Evan Azevedo <evan@azevedo.com>
2023-01-18 20:19:26 -0700

    creating endpoint to pull data from backend

--- database-api/database_api/app.py
+++ database-api/database_api/app.py
@@ -17,13 +17,19 @@ def get_db():
         db.close()
 
 
-@app.post("/create_post", response_model=schemas.PostModel)
+@app.post("/content", response_model=schemas.PostModel)
 def create_post(post: schemas.ContentModel, db: Session = Depends(get_db)):
     """Create a post in the database."""
     return crud.create_post(db, post)
 
 
-@app.post("/response")
-def record_response(post: schemas.ResponseModel, db: Session = Depends(get_db)):
+@app.get("/content/{content_id}", response_model=schemas.PostModel)
+def read_post(content_id: str, db: Session = Depends(get_db)):
+    """Read a post from the database"""
+    return crud.read_post(db, content_id)
+
+
+@app.put("/content/{content_id}", response_model=schemas.LikeModel)
+def record_response(content_id: str, like: schemas.LikeModel, db: Session = Depends(get_db)):
     """Record a response to the database."""
-    return crud.update_post_like(db, post)
+    return crud.update_post_like(db, content_id, like)
--- database-api/database_api/crud.py
+++ database-api/database_api/crud.py
@@ -1,17 +1,16 @@
 """Simple CRUD operations."""
-from logging import getLogger
+import logging
 
 from sqlalchemy.orm import Session
 
 from database_api import models, schemas
 
-LOGGER = getLogger(__name__)
-LOGGER.setLevel("INFO")
+logging.basicConfig(level="DEBUG")
 
 
 def create_post(db: Session, post: schemas.ContentModel):
     """Create a post in the database"""
-    LOGGER.info("Writing post %s", str(post))
+    logging.info("Writing post %s", str(post))
     db_post = models.Post(**post.dict())
     db.add(db_post)
     db.commit()
@@ -19,6 +18,22 @@ def create_post(db: Session, post: schemas.ContentModel):
     return db_post
 
 
-def update_post_like(db: Session, response: schemas.ResponseModel):
-    """Update a post with the user's response."""
-    return db.query(models.Post).filter(models.Post.id == response.id).update({"like": response.like})
+def read_post(db: Session, content_id: str):
+    """Read a post from the database.
+
+    :param db: The database session.
+    :param content_id: The post to read from the database.
+    """
+    query = db.query(models.Post).filter(models.Post.id == content_id)
+    post = db.scalars(query).all()[0]
+    logging.info("Post: %s", post)
+    return post
+
+
+def update_post_like(db: Session, content_id: str, like: schemas.LikeModel):
+    """Update a post with the user's response.
+
+    :param content_id: The ID of the content to update.
+    :param like: The like value to set. Either True or False.
+    """
+    return db.query(models.Post).filter(models.Post.id == content_id).update({"like": like.like})
