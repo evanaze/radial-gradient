9163ad9c8a28270fd4bf7f6db279ec875bf9b70e
Evan Azevedo <evan@azevedo.com>
2023-01-19 08:26:48 -0700

    creating frontent

---
+++ rg-frontend/src/App.js
@@ -0,0 +1,22 @@
+import './App.css';
+import 'bootstrap/dist/css/bootstrap.min.css';
+
+function App() {
+  return (
+    <div class="container">
+
+      <div class="card">
+        <div class="card-body text-center">
+          <h5 class="card-title">Card title</h5>
+          <h6 class="card-subtitle mb-2 text-muted">Card subtitle</h6>
+          <p class="card-text">Some quick example text to build on the card title and make up the bulk of the card's
+            content.</p>
+          <a href="#" class="card-link">👍</a>
+          <a href="#" class="card-link">👎</a>
+        </div>
+      </div>
+    </div>
+  );
+}
+
+export default App;
