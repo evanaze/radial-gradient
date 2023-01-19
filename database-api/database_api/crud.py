ac0231f54082c3f9dc96b4aa8c93e77ee40e291b
Evan Azevedo <evan@azevedo.com>
2023-01-18 20:20:17 -0700

    removing logger in backend

--- database-api/database_api/crud.py
+++ database-api/database_api/crud.py
@@ -1,16 +1,11 @@
 """Simple CRUD operations."""
-import logging
-
 from sqlalchemy.orm import Session
 
 from database_api import models, schemas
 
-logging.basicConfig(level="DEBUG")
-
 
 def create_post(db: Session, post: schemas.ContentModel):
     """Create a post in the database"""
-    logging.info("Writing post %s", str(post))
     db_post = models.Post(**post.dict())
     db.add(db_post)
     db.commit()
@@ -25,9 +20,7 @@ def read_post(db: Session, content_id: str):
     :param content_id: The post to read from the database.
     """
     query = db.query(models.Post).filter(models.Post.id == content_id)
-    post = db.scalars(query).all()[0]
-    logging.info("Post: %s", post)
-    return post
+    return db.scalars(query).all()[0]
 
 
 def update_post_like(db: Session, content_id: str, like: schemas.LikeModel):
